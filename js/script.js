ea13341f8e3258cb38d234e3a21ea13e48d81405
kharper2 <kharper2@hawk.iit.edu>
2022-11-04 20:53:11 -0500

    fixed function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init(){
-//add your javascrip between these two lines of code
-
-
-
-
-
-
-
-
+  function al(){
+    var e = document.getElementById('entryinput');
+    var t = document.getElementById('textoutput');
+    t.innerHTML = e.value;
+    alert('Kathryn Harper: ' + e.value);
+  }
+  var b = document.getElementById('entrybutton');
+  b.addEventListener('click', al);
+}
 window.addEventListener('load', init);
